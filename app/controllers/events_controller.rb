ee4b2b7555c8bf849e58bbe463dcad27a5d6163e
kreamacbea <kreamacbea@gmail.com>
2017-09-05 16:15:45 +0200

    Add Event Views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,51 +1,51 @@
 class EventsController < ApplicationController
 
   before_action :set_event, only: [:show, :edit, :update]
-    before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show]
 
-    def index
-      @events = current_user.events
+  def index
+    @events = current_user.events
+  end
+
+  def show; end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event, notice: "Event created"
+    else
+      render :new
     end
+  end
 
-    def show; end
+  def edit; end
 
-    def new
-      @event = current_user.events.build
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: "Event updated"
+    else
+      render :edit
     end
+  end
 
-    def create
-      @event = current_user.events.build(event_params)
+  private
 
-      if @event.save
-        redirect_to @event, notice: "Event created"
-      else
-        render :new
-      end
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
-    def edit; end
-
-    def update
-      if @event.update(event_params)
-        redirect_to @event, notice: "Event updated"
-      else
-        render :edit
-      end
-    end
-
-    private
-
-    def set_event
-      @event = Event.find(params[:id])
-    end
-
-    def event_params
-      params
-        .require(:event)
-        .permit(
-          :name, :description, :location, :price, :capacity, :includes_food,
-          :includes_drinks, :starts_at, :ends_at, :active
-        )
-    end
+  def event_params
+    params
+      .require(:event)
+      .permit(
+        :name, :description, :location, :price, :capacity, :includes_food,
+        :includes_drinks, :starts_at, :ends_at, :active
+      )
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+laura = User.create(email: "laura@codaisseurup.com", password: "ikbengek")
+
+event = Event.create(name: "Sokkenbreier", description: "Leer hoe je lekkere warme wollen sokken kunt breien", location: "Den Haag", price: 24.95, capacity: 15, includes_food: true, includes_drinks: true, starts_at: 2017-09-15T03:05:06+07:00, ends_at: 2017-09-15T04:05:06+07:00, active: true, user: laura)
