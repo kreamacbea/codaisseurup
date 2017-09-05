9f571bde4cbc78638bf61ce6540b5b6cc309d38e
kreamacbea <kreamacbea@gmail.com>
2017-09-05 12:44:37 +0200

    Add validations to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,11 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true, if: :one_day_after?
+
+  def one_day_after?
+    :ends_at >= :starts_at + 1
+  end
+
 end
