77c13f004652bb1ce0a6b8e1e4d908541bde3075
kreamacbea <kreamacbea@gmail.com>
2017-09-05 13:48:40 +0200

    One-to-Many: Controllers

---
+++ db/migrate/20170905105747_add_default_values_to_events.rb
@@ -0,0 +1,10 @@
+class AddDefaultValuesToEvents < ActiveRecord::Migration[5.1]
+  def change
+    change_table :table do |t|
+      t.change_default(:price, '0')
+      t.change_default(:includes_food, false)
+      t.change_default(:includes_drinks, false)
+      t.change_default(:active, true)
+    end
+  end
+end
