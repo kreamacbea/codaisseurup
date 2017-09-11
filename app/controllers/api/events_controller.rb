3b915d67ac137996140ef8b802b1f6dcdd85c6da
kreamacbea <kreamacbea@gmail.com>
2017-09-11 16:03:12 +0200

    Add API events destroy and update

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -29,6 +29,31 @@ class Api::EventsController < ApplicationController
       }.to_json
     end
   end
+
+  def update
+    event = Event.find(params[:id])
+    if event.update(event_params)
+      render status: 200, json: {
+        message: "Event successfully updated",
+        event: event
+      }.to_json
+    else
+       render status: 422, json: {
+        message: "The event could not be updated",
+        errors: event.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+
+    render status: 200, json: {
+      message: "Event successfully deleted"
+    }.to_json
+  end
+
   private
 
   def event_params
